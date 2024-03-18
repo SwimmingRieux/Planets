21a4cee313f35698cb7aab145c0346e86f155e0c
SwimmingRieux <141845753+SwimmingRieux@users.noreply.github.com>
2024-03-19 00:01:12 +0330

    Update planets.cpp

--- planets.cpp
+++ planets.cpp
@@ -1,103 +1,99 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <ncurses.h>
 #include "Planet.h"
 #include "TwoDim.h"
 using namespace std;
-#define deb(x) cout<<#x<<":"<<x<<"\n"
-#define in(x) cin>>x
+#define deb(x) cout << #x << ":" << x << "\n"
 #define pb push_back
 #define ppb pop_back
-#define pii pair<ll, ll>
-#define piii pair<ll, pii>
-#define f first
-#define s second
 #define ll long long
-#define per(i,a,b) for(ll i=a;i<=b;i++)
-#define rep(i,a,b) for(ll i=a;i>=b;i--)
-#define outl(x) cout<<x<<endl;
-#define outc(x) cout<<x<<" ";
+#define double long double
+#define per(i, a, b) for (ll i = a; i <= b; i++)
+#define rep(i, a, b) for (ll i = a; i >= b; i--)
 const ll maxn = 1e3 + 15;
-const ll mod = 1e9+7;
-const ll lg = 22;
 
 vector<vector<TwoDim>> Distances, Forces;
 vector<Planet> planets;
 vector<TwoDim> markedCells;
-int planets_count, forces_coefficient,  velocity_coefficient;
+ll planets_count, forces_coefficient, velocity_coefficient;
 bool flg[maxn][maxn], game1 = 1, game2 = 1;
 double planets_density;
-int maxL, maxC, maxR, minR;
+ll maxL, maxC, maxR, minR;
 mutex mtx;
-int genRand(int a, int b) {
+ll genRand(ll a, ll b)
+{
     random_device rd;
     static mt19937 gen(rd());
     static uniform_int_distribution<> distrib(a, b);
-    return ((distrib(gen))%(b-a+1))+a;
+    return ((distrib(gen)) % (b - a + 1)) + a;
 }
 
-void init(int n, double p, int fc, int vc, int mnr, int mxr)
+void init(ll n, double p, ll fc, ll vc, ll mnr, ll mxr)
 {
     vector<TwoDim> tmp;
-    per(i,0,maxn-1) tmp.pb(TwoDim(0, 0));
-    per(i,0,maxn-1) { Forces.pb(tmp); Distances.pb(tmp); }
+    per(i, 0, maxn - 1) tmp.pb(TwoDim(0, 0));
+    per(i, 0, maxn - 1)
+    {
+        Forces.pb(tmp);
+        Distances.pb(tmp);
+    }
     forces_coefficient = fc;
     velocity_coefficient = vc;
     planets.pb(Planet(0, 0, 0, 0, 0, 0, 0, 0));
-    getmaxyx(stdscr, maxL, maxC);
-    maxL--; maxC--;
-    maxR = mxr; minR = mnr;
+    getmaxyx(stdscr, maxC, maxL);
+    maxL--;
+    maxC--;
+    maxR = mxr;
+    minR = mnr;
     planets_density = p;
     planets_count = n;
-    per(i,1,n)
+    per(i, 1, n)
     {
         bool flg = 1;
-        int randx = genRand(2,maxL-2);
-        int randy = genRand(2,maxC-2);
-        int randr = genRand(minR,maxR);
-        while(flg)
+        ll randx = genRand(2, maxC - 2);
+        ll randy = genRand(2, maxL - 2);
+        ll randr = genRand(minR, maxR);
+        while (flg)
         {
             flg = 0;
-            per(j,1,i-1)
+            per(j, 1, i - 1)
             {
-                if(randx == planets[j].getCoordinate().x && randy == planets[j].getCoordinate().y)
+                if (randx == planets[j].getCoordinate().x && randy == planets[j].getCoordinate().y)
                 {
                     flg = 1;
-                    randx = genRand(2,maxL-2);
-                    randy = genRand(2,maxC-2);
+                    randx = genRand(2, maxC - 2);
+                    randy = genRand(2, maxL - 2);
                     break;
                 }
             }
-            if(!flg)
+            if (!flg)
             {
-                if(randx + randr > maxL - 1 || randx - randr < 2 || randy + randr > maxC - 1 || randy - randr < 2)
+                if (randx + randr > maxC - 1 || randx - randr < 2 || randy + randr > maxL - 1 || randy - randr < 2)
                 {
                     flg = 1;
-                    randx = genRand(2,maxL-2);
-                    randy = genRand(2,maxC-2);
-                    randr = genRand(minR,maxR);
+                    randx = genRand(2, maxC - 2);
+                    randy = genRand(2, maxL - 2);
+                    randr = genRand(minR, maxR);
                 }
             }
         }
-        //Planet(double vx, double vy, int cx, int cy, double fx, double fy, int t, double r) :
-        //    velocity(vx, vy), coordinate(cx, cy), force(fx, fy), time(t), radius(r) {}
-
-        planets.pb(Planet(/*genRand(1,20)*/0, /*genRand(1,20)*/0, randx, randy, 0, 0, 0, randr));
+        planets.pb(Planet(/*genRand(1,20)*/ 0, /*genRand(1,20)*/ 0, randx, randy, 0, 0, 0, randr));
     }
 }
 
-void drawPlanet(int tmpi, int tmpj, const Planet& planet)
+void drawPlanet(ll tmpi, ll tmpj, const Planet &planet)
 {
     mvaddch(tmpi, tmpj, '*');
     flg[tmpi][tmpj] = 1;
     markedCells.pb(TwoDim(tmpi, tmpj));
-    per(i,-1,1)
+    per(i, -1, 1)
     {
-        per(j,-1,1)
+        per(j, -1, 1)
         {
-            
-            if(abs(i+j) % 2 == 1 && !flg[tmpi+i][tmpj+j] && sqrt(pow(tmpi+i - planet.getCoordinate().x, 2) + pow(tmpj+j - planet.getCoordinate().y, 2) <= planet.getRadius()))
+
+            if (abs(i + j) % 2 == 1 && !flg[tmpi + i][tmpj + j] && sqrt(pow(tmpi + i - planet.getCoordinate().x, 2) + pow(tmpj + j - planet.getCoordinate().y, 2) <= planet.getRadius()))
             {
-                drawPlanet(tmpi+i, tmpj+j, planet);
+                drawPlanet(tmpi + i, tmpj + j, planet);
             }
         }
     }
@@ -106,12 +102,23 @@ void drawPlanet(int tmpi, int tmpj, const Planet& planet)
 void draw()
 {
     clear();
-    per(i,1,planets_count)
+    per(i, 0, maxL - 1)
+    {
+        mvaddch(maxC, i, '#');
+        mvaddch(0, i, '#');
+    }
+
+    per(i, 0, maxC - 1)
+    {
+        mvaddch(i, maxL, '#');
+        mvaddch(i, 0, '#');
+    }
+    per(i, 1, planets_count)
     {
         drawPlanet(planets[i].getCoordinate().x, planets[i].getCoordinate().y, planets[i]);
-        for(int j = markedCells.size()-1; j>=0; j--)
+        for (ll j = markedCells.size() - 1; j >= 0; j--)
         {
-            flg[(int)markedCells[j].x][(int)markedCells[j].y] = 0;
+            flg[(ll)markedCells[j].x][(ll)markedCells[j].y] = 0;
             markedCells.ppb();
         }
     }
@@ -120,86 +127,104 @@ void draw()
 
 void setVariables()
 {
-    per(i,1,planets_count)
+    per(i, 1, planets_count)
     {
-        per(j,i+1,planets_count)
+        per(j, i + 1, planets_count)
         {
-            int xi = planets[i].getCoordinate().x;
-            int yi = planets[i].getCoordinate().y;
-            int xj = planets[j].getCoordinate().x;
-            int yj = planets[j].getCoordinate().y;
+            ll xi = planets[i].getCoordinate().x;
+            ll yi = planets[i].getCoordinate().y;
+            ll xj = planets[j].getCoordinate().x;
+            ll yj = planets[j].getCoordinate().y;
             double ri = planets[i].getRadius();
             double rj = planets[j].getRadius();
-            double mi = pow(ri, 2)*planets_density;
-            double mj = pow(rj, 2)*planets_density;
+            double mi = pow(ri, 2) * planets_density;
+            double mj = pow(rj, 2) * planets_density;
             mi /= forces_coefficient;
             mj /= forces_coefficient;
-            int dxij = abs(xi - xj);
-            int dyij = abs(yi - yj);
+            ll dxij = xi - xj;
+            ll dyij = yi - yj;
             Distances[i][j] = Distances[j][i] = TwoDim(dxij, dyij);
-            Forces[i][j] = Forces[j][i] = TwoDim(mi*mj/pow(dxij, 2), mi*mj/pow(dyij, 2));
-            deb(mi);
-            deb(mj);
-            deb(Forces[i][j].x);
-            deb(Forces[i][j].y);
+            Forces[i][j] = Forces[j][i] = TwoDim(mi * mj / (dxij * abs(dxij)), mi * mj / (dyij * abs(dyij)));
         }
     }
-    per(i,1,planets_count)
+    per(i, 1, planets_count)
     {
         planets[i].setForce(TwoDim(0, 0));
-        per(j,1,planets_count) if(i!=j)
+        per(j, 1, planets_count) if (i != j)
         {
             double Fxi = planets[i].getForce().x;
             double Fyi = planets[i].getForce().y;
             planets[i].setForce(TwoDim(Fxi + Forces[i][j].x, Fyi + Forces[i][j].y));
         }
-        double mi = (3.14)*pow(planets[i].getRadius(), 2)*planets_density;
-        TwoDim Fi = planets[i].getForce();
+        
+        double mi = pow(planets[i].getRadius(), 2) * planets_density;
         TwoDim vi = planets[i].getVelocity();
         TwoDim xi = planets[i].getCoordinate();
-        //deb(minVelX);
-        //deb(minVelY);
-        planets[i].setVelocity(TwoDim(vi.x + (Fi.x/mi)/velocity_coefficient, vi.y + (Fi.y/mi)/velocity_coefficient));
-        //planets[i].setVelocity(TwoDim((vi.x + min(minVelX, (Fi.x/mi)/10000)), (vi.y + min(minVelY, Fi.y/mi)/10000)));
-        vi = planets[i].getVelocity();
-        //deb(planets[i].getVelocity().x);
-        //deb(planets[i].getVelocity().y);
-        planets[i].setCoordinate(TwoDim(xi.x + vi.x, xi.y + vi.y));
-        // here
-    }
+        per(j, 0, maxL - 1)
+        {
+            double Fxi = planets[i].getForce().x;
+            double Fyi = planets[i].getForce().y;
+            Fxi -= (mi/forces_coefficient)/((maxC - xi.x) * abs(maxC - xi.x)); 
+            Fyi -= (mi/forces_coefficient)/((i - xi.y) * abs(i - xi.y));
+            Fxi -= (mi/forces_coefficient)/((0 - xi.x) * abs(0 - xi.x)); 
+            Fyi -= (mi/forces_coefficient)/((i - xi.y) * abs(i - xi.y));
+            planets[i].setForce(TwoDim(Fxi, Fyi));
+        }
 
+        per(j, 0, maxC - 1)
+        {
+            double Fxi = planets[i].getForce().x;
+            double Fyi = planets[i].getForce().y;
+            Fxi -= (mi/forces_coefficient)/((i - xi.x) * abs(i - xi.x)); 
+            Fyi -= (mi/forces_coefficient)/((maxL - xi.y) * abs(maxL - xi.y));
+            Fxi -= (mi/forces_coefficient)/((i - xi.x) * abs(i - xi.x)); 
+            Fyi -= (mi/forces_coefficient)/((0 - xi.y) * abs(0 - xi.y));
+            planets[i].setForce(TwoDim(Fxi, Fyi));
+        }
+
+
+
+
+
+
+        TwoDim Fi = planets[i].getForce();
+        planets[i].setVelocity(TwoDim(vi.x + (Fi.x / mi) / velocity_coefficient, vi.y + (Fi.y / mi) / velocity_coefficient));
+        vi = planets[i].getVelocity();
+        planets[i].setCoordinate(TwoDim(xi.x + vi.x, xi.y + vi.y));
+    }
 }
 
 void readThread()
 {
     noecho();
-    while(game2)
+    while (game2)
     {
 
         auto start = std::chrono::high_resolution_clock::now();
         mtx.lock();
         char c = getch();
-        if(c == 's') 
+        if (c == 's')
         {
-            planets[1].setRadius(max(planets[1].getRadius()-1,(double)minR));
+            planets[1].setRadius(max(planets[1].getRadius() - 1, (double)minR));
         }
-        else if(c == 'w')
+        else if (c == 'w')
         {
-            planets[1].setRadius(min(planets[1].getRadius()+1,(double)maxR));
+            planets[1].setRadius(min(planets[1].getRadius() + 1, (double)maxR));
         }
-        else if(c == 'q') game1 = game2 = 0;
+        else if (c == 'q')
+            game1 = game2 = 0;
         mtx.unlock();
         auto stop = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-        //deb(duration.count());
+        // deb(duration.count());
         this_thread::sleep_for(std::chrono::milliseconds(25 - duration.count()));
     }
 }
 
 void performThread()
 {
-    //deb("perform");
-    while(game1)
+    // deb("perform");
+    while (game1)
     {
         mtx.lock();
         setVariables();
@@ -209,19 +234,17 @@ void performThread()
     }
 }
 
-
 signed main()
-{    
+{
     game1 = game2 = 1;
     initscr();
-    init(3, 5, 5, 50, 5, 20);
+    init(1, 5, 10, 10, 3, 10);
     timeout(24);
-    //nodelay(stdscr, TRUE);
+    // nodelay(stdscr, TRUE);
     thread t1(performThread);
     thread t2(readThread);
     t1.join();
     t2.join();
     endwin();
-    
     return 0;
 }
